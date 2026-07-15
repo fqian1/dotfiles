492e0175e393d6601dfae4b01f92c0770f698cbb
Francois Qian <francois.qian2@gmail.com>
2026-07-15 20:43:24 +0000

    update

---
+++ home/.config/vis/visrc.lua
@@ -0,0 +1,24 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+	-- Your per window configuration options e.g.
+	-- vis:command('set number')
+end)
+
+local plug = (function() if not pcall(require, 'plugins/vis-plug') then
+	os.execute('git clone --quiet https://github.com/erf/vis-plug ' ..
+		(os.getenv('XDG_CONFIG_HOME') or os.getenv('HOME') .. '/.config')
+		.. '/vis/plugins/vis-plug')
+end return require('plugins/vis-plug') end)()
+
+local plugins = {
+	{ erf/vis-cursors },
+	{ ~mcepl/vis-fugitive },
+}
+
+require('plugins/vis-plug').init(plugins, true)
